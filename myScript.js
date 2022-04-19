984cecdc117c095c867b2e941858696b1b66fed7
Zeyuurr <103393898+Zeyuurr@users.noreply.github.com>
2022-04-19 11:16:55 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,8 @@
+//Zain(Chen Zeyu)
 function myFunction(){
-    var x = document.getElementById("myText").value
-    var y = document.getElementById("myText1").value
-    if(isNaN(x)|isNaN(y)|x==""|y==""|x=="null"|y=="null"){
+    var x = document.getElementById("myText").value//Get the value of x
+    var y = document.getElementById("myText1").value//Get the value of y
+    if(isNaN(x)|isNaN(y)|x==""|y==""|x=="null"|y=="null"){//Determines whether x and y are numbers. If they are numbers, add them together. If they are not numbers, return the user to enter a number
      document.getElementById("demo").innerHTML = "Please enter two numbers"
     }
     else{   
